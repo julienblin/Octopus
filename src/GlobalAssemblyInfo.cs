40d5f34603ea9369f3aa37fa5353f0f908136ac5
Julien Blin <julien.blin@gmail.com>
2011-12-23 17:07:53 -0500

    first commit

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("julien")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
---
+++ src/Octopus.Tests/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Octopus.Tests")]
+[assembly: AssemblyDescription("")]
+
